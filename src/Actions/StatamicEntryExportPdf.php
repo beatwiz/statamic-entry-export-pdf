6b96de98f0ff6713e3d3fe130a9eb333d954a2a2
Gustavo Silva <beatwiz@users.noreply.github.com>
2023-02-08 21:35:36 +0000

    Create StatamicEntryExportPdf.php

---
+++ src/Actions/StatamicEntryExportPdf.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Beatwiz\StatamicEntryExportPdf\Actions;
+
+use Statamic\Actions\Action;
+
+class StatamicEntryExportPdf extends Action
+{
+    /**
+     * Title
+     */
+    public static function title()
+    {
+        return __('Export PDF');
+    }
+    /**
+     * The run method
+     *
+     * @return void
+     */
+    public function run($items, $values)
+    {
+        // Your code
+    }
+}
