cd0394d320b6f481595d79960b87effbf88e5506
Gustavo Silva <beatwiz@users.noreply.github.com>
2023-02-08 21:23:26 +0000

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace beatwiz\EntryExportPDF;
+namespace Beatwiz\StatamicEntryExportPdf;
 
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    public function register()
+    protected $actions = [
+        Actions\StatamicEntryExportPdf::class,
+    ];
+    public function bootAddon()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/statamic-entry-export-pdf.php', 'statamic-entry-export-pdf');
 
         $this->publishes([
             __DIR__ . '/../config/statamic-entry-export-pdf.php' => config_path('statamic-entry-export-pdf.php'),
         ], 'statamic-entry-export-pdf-config');
-
-        EntryExportPDF::register();
     }
 }
+
