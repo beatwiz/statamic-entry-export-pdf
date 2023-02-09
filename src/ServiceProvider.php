738ae3a48d77b89493183f4defb15bbc0b9bb1ba
Gustavo Silva <8360167+beatwiz@users.noreply.github.com>
2023-02-09 15:43:33 +0000

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,6 +9,7 @@ class ServiceProvider extends AddonServiceProvider
     protected $actions = [
         Actions\StatamicEntryExportPdf::class,
     ];
+
     public function bootAddon()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/statamic-entry-export-pdf.php', 'statamic-entry-export-pdf');
@@ -16,6 +17,9 @@ class ServiceProvider extends AddonServiceProvider
         $this->publishes([
             __DIR__ . '/../config/statamic-entry-export-pdf.php' => config_path('statamic-entry-export-pdf.php'),
         ], 'statamic-entry-export-pdf-config');
+
+        $this->publishes([__DIR__ . '/../resources/views' => base_path('resources/views/vendor/statamic-entry-export-pdf')], 'statamic-entry-export-pdf-views');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'statamic-entry-export-pdf');
     }
 }
-
