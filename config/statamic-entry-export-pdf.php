4f88650ad3f3fef8a73be3a318a8897fe48b6481
Gustavo Silva <8360167+beatwiz@users.noreply.github.com>
2023-02-09 15:21:13 +0000

    Update statamic-entry-export-pdf.php

--- config/statamic-entry-export-pdf.php
+++ config/statamic-entry-export-pdf.php
@@ -4,5 +4,11 @@ return [
     'excluded_field_names' => [
       // "example",
     ],
-    'excluded_field_types' => ['section', 'hidden']
+    'excluded_field_types' => [
+        'section',
+        'hidden'
+    ],
+    'excluded_collections' => [
+      // "example",
+    ]
 ];
